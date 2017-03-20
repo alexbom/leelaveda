5cfb953b15a4d9eb2d4b0eefefad14f1f706c7cb
alexbom <gush@bk.ru>
2017-03-20 11:37:39 +0300

    save load

--- php/save.php
+++ php/save.php
@@ -1,7 +1,7 @@
-<?php
+<?php echo '!';die();
 header("Access-Control-Allow-Origin: *");
 
 $name = md5($_SERVER['REMOTE_ADDR'].time());
 $file = '../game/'.$name.'.json';
-var_dump($_POST);
+echo $_POST['game'];
 if (file_put_contents($file, $_POST['game'], FILE_APPEND)) echo $name;
