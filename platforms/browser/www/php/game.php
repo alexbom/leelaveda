3126fbd5561c2bb01adf32049ba62b9260810fd8
alexbom <a.bom@hotelbook.ru>
2017-04-08 18:45:59 +0300

    refactored

--- platforms/browser/www/php/game.php
+++
@@ -1,8 +0,0 @@
-<?php
-$html  = file('../game/'.$_GET['id'].'.txt');
-$title = 'Путь игрока';
-$descr = '';
-$js    = ',{"src" : "js/game.js", "async" : false}';
-$body  = json_encode($html);
-
-require('page.php');
--- platforms/browser/www/php/load.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-$file = '../game/'.$_GET['game_id'].'.json';
-
-echo json_encode(file($file));
